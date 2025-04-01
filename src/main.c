d009856b2e8651bc6057a28f5b1865c221426716
Kirill Tatarenko <kirill.tatarenko@rockbitegames.com>
2025-04-01 13:42:12 +0400

    process: 2

--- src/main.c
+++ src/main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+// directive declaration
+#define INCHES_PER_POUND 166 
 
-#define INCHES_PER_POUND 166 // directive declaration
-
-void one(); // function declaration
+// function declaration
+void one(); 
 void two();
+void three();
+void four();
 
 int main () {
-    printf("Hello world!. I wrote my firs program ic C!\n");
-
-    one();
-    two();
+    // one();
+    // two();
+    // three();
+    four();
 
 
     return 0;
@@ -18,6 +21,7 @@ int main () {
 void one() {
     int integer = 2;
     float floatingPoint = 5.0256f;
+
     printf("An integer number is: %d, a floating point number is: %.2f\n", integer, floatingPoint); // 0.nf - round a number to n chars
 }
 
@@ -36,4 +40,23 @@ void two() {
 
     printf("Volume (cubic inches): %d\n", volume);
     printf("Dimensional weight (pounds): %d\n", weight);
+}
+
+void three() {
+    int one;
+    float two, three;
+
+    printf("Give me some numbers fella: ");
+    scanf("%d%f%f", &one, &two, &three);
+    printf("Your numbers are: %d | %f | %f\n", one, two, three);
+}
+
+
+void four() {
+    int one, two;
+
+    printf("Give me some numbers fella: ");
+    scanf("%d / %d", &one, &two); // the amount of whitespaces does mot make a difference, the existence is matter
+    printf("Your numbers are:\t %d | %d\n", one, two);
+    printf("Numbers are:\t\t %d | %d\n", one, two);
 }
